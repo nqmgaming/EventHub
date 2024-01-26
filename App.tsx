1d289822d195b627da894d0aaab780cabfb583d3
Nguyễn Quang Minh (NQM) <minhnqph31902@fpt.edu.vn>
2024-01-27 02:18:09 +0700

    Add AsyncStorage functionality for asset token

--- App.tsx
+++ App.tsx
@@ -3,15 +3,36 @@ import React, {useEffect, useState} from 'react';
 import {SplashScreen} from './src/screens';
 import AuthNavigator from './src/navigators/AuthNavigator';
 import {NavigationContainer} from '@react-navigation/native';
+import {useAsyncStorage} from '@react-native-async-storage/async-storage';
+import MainNavigator from './src/navigators/MainNavigator';
 
 const App = () => {
   const [isShowSplash, setisShowSplash] = useState(true);
+  const [assetToken, setassetToken] = useState('');
+  const {getItem, setItem} = useAsyncStorage('assetToken');
+
   useEffect(() => {
     const timeout = setTimeout(() => {
       setisShowSplash(false);
     }, 1500);
     return () => clearTimeout(timeout);
   }, []);
+
+  useEffect(() => {
+    checkLogin();
+  }, []);
+
+  const checkLogin = async () => {
+    const token = await getItem();
+    console.log('====================================');
+    console.log('token', token);
+    console.log('====================================');
+    if (token) {
+      setassetToken(token);
+    } else {
+      setassetToken('');
+    }
+  };
   return (
     <>
       <StatusBar
@@ -23,7 +44,7 @@ const App = () => {
         <SplashScreen />
       ) : (
         <NavigationContainer>
-          <AuthNavigator />
+          {assetToken ? <MainNavigator /> : <AuthNavigator />}
         </NavigationContainer>
       )}
     </>
--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,10 +1,21 @@
-import {StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import {Button, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Hello</Text>
+      <Button
+        title="Login"
+        onPress={async () => await AsyncStorage.setItem('assetToken', 'hello')}
+      />
     </View>
   );
 };
