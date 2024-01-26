f3a6e4eede458b678013bed582d50efb84884faf
Nguyễn Quang Minh (NQM) <minhnqph31902@fpt.edu.vn>
2024-01-27 01:21:04 +0700

    Refactor code for improved performance

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import {StyleSheet} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {SplashScreen} from './src/screens';
+import AuthNavigator from './src/navigators/AuthNavigator';
+import {NavigationContainer} from '@react-navigation/native';
+
+const App = () => {
+  const [isShowSplash, setisShowSplash] = useState(true);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setisShowSplash(false);
+    }, 1500);
+    return () => clearTimeout(timeout);
+  }, []);
+  return isShowSplash ? (
+    <SplashScreen />
+  ) : (
+    <NavigationContainer>
+      <AuthNavigator />
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ src/constants/appColors.ts
@@ -0,0 +1,7 @@
+export const appColors = {
+  primary: '#3D56F0',
+  white: '#FFFFFF',
+  text: '#120D26',
+  gray: '#E4DFDF',
+  darkGray: '#807A7A',
+};
